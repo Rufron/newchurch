0c62d948675455b88e07e7709599498e44fc3025
Rufron <kipchumbabrian572@gmail.com>
2024-07-22 11:22:30 +0300

    fourteenth commit

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -23,6 +23,9 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container px-4 px-lg-5">
+                <a href="/" class="logo">
+                    <img src="https://media.istockphoto.com/id/1333675089/vector/church-christian-vector-icon-on-white-background.jpg?s=1024x1024&w=is&k=20&c=CmUGRFYHCcnjE2XqMfRpHRJfyXgnXTFfqrsv_JR1na0=" alt="Your Logo">
+                  </a>
                 <a class="navbar-brand" href="#page-top">True Church</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
